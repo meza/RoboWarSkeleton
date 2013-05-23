cdf1ff7ae6b69596ddfd915ae26d606bf51f60c2
meza <meza@meza.hu>
2013-05-23 10:47:35 +0200

    costs config

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,17 +2,26 @@ import hu.meza.robowars.Arena;
 import hu.meza.robowars.ArenaMaster;
 import hu.meza.robowars.DefaultArena;
 import hu.meza.robowars.IdGenerator;
+import hu.meza.robowars.MovementCosts;
 import hu.meza.robowars.Robot;
 import robots.RandomRobot;
 
 public class Runner {
 
-	public static final int SIZE = 64;
+	private static final int SIZE = 64;
+	private static final int MOVE = 250;
+	private static final int TURN = 500;
+	private static final int SCAN = 300;
+	private static final int TAG = 1500;
+	private static final int CLONE = 2500;
 
 	public static void main(String[] args) {
 
-		Arena arena = new DefaultArena(SIZE);
-		ArenaMaster master = new ArenaMaster(arena, new IdGenerator());
+
+		MovementCosts costs = new MovementCosts(MOVE, TURN, SCAN, TAG, CLONE);
+
+		Arena arena = new DefaultArena(SIZE, costs);
+		ArenaMaster master = new ArenaMaster(arena, new IdGenerator(), costs);
 
 		Robot robot1 = new RandomRobot("Jack", "WARRIORS", "Jack");
 		Robot robot2 = new RandomRobot("Jill", "PRINCESSES", "Jill");
@@ -23,7 +32,6 @@ public class Runner {
 
 
 		master.startGame();
-
 
 	}
 }
